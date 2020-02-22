1b6b40130b86570185832a72ffd19508f463fad9
Igor Rebrishchev <igorr28@yandex.ru>
2020-02-23 00:32:58 +0300

    made dz6.1-6.2

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@
    delayPromise(3) // вернет promise, который будет разрешен через 3 секунды
  */
 function delayPromise(seconds) {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve(), seconds*1000);
+    });
 }
 
 /*
@@ -25,6 +28,24 @@ function delayPromise(seconds) {
    loadAndSortTowns().then(towns => console.log(towns)) // должна вывести в консоль отсортированный массив городов
  */
 function loadAndSortTowns() {
+    return new Promise((resolve) => {
+        fetch('https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json')
+            .then(response => response.json())
+            .then(towns => {
+                towns.sort((a, b) => {
+                    if (a.name > b.name) {
+                        return 1; 
+                    }
+                    if (a.name === b.name) {
+                        return 0; 
+                    } 
+                    if (a.name < b.name) {
+                        return -1; 
+                    } 
+                });
+                resolve(towns);
+            });
+    });
 }
 
 export {
